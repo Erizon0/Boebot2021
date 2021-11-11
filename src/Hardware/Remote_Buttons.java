a62dde6d51559509d0dec962c230410e0ee258c8
Erizon <dragonwarrior66.rr@gmail.com>
2021-11-11 14:00:22 +0100

    Code setup

---
+++ src/Hardware/Remote_Buttons.java
@@ -0,0 +1,38 @@
+package Hardware;
+
+public class Remote_Buttons {
+    public static final int BUTTON_POWER = 10010101;
+    public static final int BUTTON_CH_UP = 10010000;
+    public static final int BUTTON_MUTE = 10010100;
+    public static final int BUTTON_VOL_DOWN = 10010011;
+    public static final int BUTTON_VOL_UP = 10010010;
+    public static final int BUTTON_CH_WEIRD = 111001000;
+    public static final int BUTTON_CH_DOWN = 10010001;
+    public static final int BUTTON_TV_VCR = 10100101;
+    public static final int BUTTON_1 = 10000000;
+    public static final int BUTTON_2 = 10000001;
+    public static final int BUTTON_3 = 10000010;
+    public static final int BUTTON_4 = 10000011;
+    public static final int BUTTON_5 = 10000100;
+    public static final int BUTTON_6 = 10000101;
+    public static final int BUTTON_7 = 10000110;
+    public static final int BUTTON_8 = 10000111;
+    public static final int BUTTON_9 = 10001000;
+    public static final int BUTTON_ENTER = 10001100;
+    public static final int BUTTON_0 = 10001001;
+    public static final int BUTTON_STOP = 111001101;
+    public static final int BUTTON_REC = 10011101;
+    public static final int BUTTON_WHITE_BOX = 10111000;
+    public static final int BUTTON_REWIND = 111001100;
+    public static final int BUTTON_PLAY = 111001110;
+    public static final int BUTTON_FORWARD = 111001111;
+    public static final int BUTTON_TELETEXT = 10111111;
+    public static final int BUTTON_UP = 10011110;
+    public static final int BUTTON_FORMAT = 111001010;
+    public static final int BUTTON_LEFT = 10011011;
+    public static final int BUTTON_RIGHT = 10011010;
+    public static final int BUTTON_MENU = 11100000;
+    public static final int BUTTON_DOWN = 10011111;
+    public static final int BUTTON_EXIT = 111001001;
+    public static final int INVALID = 00000000000;
+}
