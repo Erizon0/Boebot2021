0d72f57a31b2ac895fd91c63605c227e444ff824
Erizon <dragonwarrior66.rr@gmail.com>
2021-11-24 14:21:33 +0100

    Led class edited

--- src/Led.java
+++ src/Led.java
@@ -1,19 +1,44 @@
 import TI.BoeBot;
+import TI.Timer;
 
 import java.awt.*;
 
 public class Led implements Updatable {
 
     private int pin;
+    private Color color;
+    private Timer timer1;
 
-    public Led(int pin) {
+    private boolean state;
+
+
+    public Led(int pin, Color color) {
         this.pin = pin;
+        this.color = color;
+        BoeBot.rgbSet(this.pin, this.color);
     }
 
-    public void blink() {
-        //Add LED blink code
-//        BoeBot.rgbSet(this.pin, 255, 255, 255);
-        BoeBot.rgbSet(this.pin, Color.white);
+    public Led(int pin, Color color, int time){
+        this.pin = pin;
+        this.color = color;
+        this.timer1 = new Timer(time);
+        BoeBot.rgbSet(this.pin, this.color);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setTime(int time) {
+        this.timer1.setInterval(time);
+    }
+
+    public void toggle(){
+        this.state =! this.state;
+
+    }
+
+    public void ledOn(){
         BoeBot.rgbShow();
     }
 
