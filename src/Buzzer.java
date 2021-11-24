06df817c536804326493da3dbabd612624d73ecd
Erizon <dragonwarrior66.rr@gmail.com>
2021-11-24 14:35:34 +0100

    finished buzzer logic

--- src/Buzzer.java
+++ src/Buzzer.java
@@ -11,33 +11,36 @@ public class Buzzer implements Updatable {
 
     private PWM pwm;
 
-
+    //volgens mij moet je hier nog "BoeBot.freqOut();" gebruiken
     public Buzzer(int pin, int frequency) {
         this.pin = pin;
         this.frequency = frequency;
         this.pwm = new PWM(this.pin, this.frequency);
-       //volgens mij moet je hier nog "BoeBot.freqOut();" gebruiken
     }
 
-    public void startContinuousBeep () { ;
+    public Buzzer(int pin, int frequency, int interval){
+        this.pin = pin;
+        this.frequency = frequency;
+        this.pwm = new PWM(this.pin, this.frequency);
+        tBeep = new Timer(interval);
+    }
+
+    public void startBeep () { ;
         this.pwm.start();
     }
 
-    public void startBeeping(int interval){
-        this.tBeep = new Timer(interval);
-        this.pwm.start();
-    }
-
-    public void toggle(){
-        this.state =! this.state;
-    }
-
-    //May be a handy method, but it depends how we code beep()
     public void stopBeep() {
         this.pwm.stop();
     }
 
-    //Don't know what to do with this yet
+    public void toggle(){
+        this.state =! this.state;
+        if(this.state = true)
+            startBeep();
+        if(this.state = false)
+            stopBeep();
+    }
+
     @Override
     public void Update() {
         if(this.tBeep == null)
